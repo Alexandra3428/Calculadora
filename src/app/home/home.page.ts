feb132c8fbb3197692cfe44be4663f632344f86c
Alexandra3428 <saracastrillon34@gmail.com>
2024-05-26 20:17:04 -0500

    se agrega proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public nombre!: string;
+  public apellido!: string;
+  public nota1!: number;
+  public nota2!: number;
+  public nota3!: number;
+  public promedio!: number;
+  public history: any[] = [];
 
   constructor() {}
 
+  calcularPromedio() {
+    const totalNotas = this.nota1 + this.nota2 + this.nota3;
+    this.promedio = parseFloat((totalNotas / 3).toFixed(1)); // Formatea el promedio a una cifra decimal
+  }
+
+  guardarEstudiante() {
+    const estudiante = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      promedio: this.promedio
+    };
+    this.history.push(estudiante);
+    this.ordenarEstudiantes();
+    this.limpiarCampos();
+  }
+
+  ordenarEstudiantes() {
+    this.history.sort((a, b) => b.promedio - a.promedio);
+  }
+
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.nota1 = 0;
+    this.nota2 = 0;
+    this.nota3 = 0;
+    this.promedio = 0;
+  }
 }
